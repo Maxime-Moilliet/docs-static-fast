fd840a4f410014244648e4ed925ae364c6dff69c
Maxime-Moilliet <mllt.dev66@gmail.com>
2022-06-14 18:49:03 +0200

    feat : upgrade App routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "@/app/home/HomePage";
+import { Error404, ErrorBoundary } from "@/errors";
+import { Loader } from "@/components";
 
 export const App = () => {
   return (
-    <BrowserRouter basename="/app">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter basename="/app">
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 };
