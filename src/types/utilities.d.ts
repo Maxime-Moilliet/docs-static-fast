2520ec1887a5929c7d575d99c9646bb6a7d38abb
Maxime-Moilliet <mllt.dev66@gmail.com>
2022-06-14 17:42:39 +0200

    feat : utilities types any

---
+++ src/types/utilities.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Use this type to temporary bypass to `any` without writting `any`
+ * Comment the line to find where it's used
+ */
+type TODO = any;
+
+/**
+ * Use this type to use an explicit `any`
+ */
+type ExplicitAny = any;
