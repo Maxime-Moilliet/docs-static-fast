8a16fbcf96864ccaf3e703e97e0515538c163164
Maxime-Moilliet <mllt.dev66@gmail.com>
2022-06-14 18:32:17 +0200

    feat : images and AppHead

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { Providers } from "@/Providers";
 import { Box, Text } from "@chakra-ui/react";
+import Head from "next/head";
 
 const AppDevHint = () => {
   const envName =
@@ -45,9 +46,39 @@ const AppDevHint = () => {
   );
 };
 
+const AppHead = () => {
+  return (
+    <Head>
+      <title>Docs static fast</title>
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,viewport-fit=cover"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+    </Head>
+  );
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Providers>
+      <AppHead />
       <Component {...pageProps} />
       <AppDevHint />
     </Providers>
